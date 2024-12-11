2fd66786607fbbf6b53d329bdbbc542c37c3b2af
Vinay14Newbie <wjust1984@gmail.com>
2024-12-11 21:46:11 +0530

    Portfolio project

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Layout />}></Route>)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
